5f77f20500d65ffa30b8fdc2d187298f2c7dbb9c
Gosthboy <josue1010andres@gmail.com>
2025-12-18 11:05:16 -0500

    Fix: Configurando Tailwind y rutas de assets

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/perfume-store',
+  assetPrefix: '/perfume-store', // Añade esta línea también
   images: {
     unoptimized: true,
   },
