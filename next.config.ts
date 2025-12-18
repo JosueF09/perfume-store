4c6b7e7a06b84f4a487c8e123a75fbda5ee762f6
Gosthboy <josue1010andres@gmail.com>
2025-12-18 11:21:40 -0500

    Correccion de rutas para despliegue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/perfume-store',
-  assetPrefix: '/perfume-store', 
+  assetPrefix: '/perfume-store/', 
   images: {
     unoptimized: true,
   },
